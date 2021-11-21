6941b20b8c8d9d614d5f05a43620b2ef12df26d4
cjarmstrong2018 <christian.armstrong25@gmail.com>
2021-11-21 16:46:19 -0600

    Finished Finviz scraper. Ready for use

--- scrape.py
+++ scrape.py
@@ -4,10 +4,24 @@ import pandas as pd
 import numpy as np
 import requests
 
+MONTHS = {
+    'January': 1,
+    'February': 2,
+    'March': 3,
+    'April': 4,
+    'May': 5,
+    'June': 6,
+    'July': 7,
+    'August': 8,
+    'September': 9,
+    'October': 10,
+    'November': 11,
+    'December': 12
+}
+
 
 class Scraper(object):
     def __init__(self):
-        print("Starting Scraper")
         self.url = 'https://finviz.com/insidertrading.ashx'
         self.header = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36",
                        "X-Requested-With": "XMLHttpRequest"
@@ -18,7 +32,6 @@ class Scraper(object):
     def get_df(self):
 
         page = requests.get(self.url, headers=self.header)
-        print(page.status_code)
         self.df = pd.read_html(page.text, attrs={'class': 'body-table'},
                                index_col=["Date", "Ticker"], header=0,
                                parse_dates=True)[0]  # Read tables and select first from list
@@ -33,53 +46,70 @@ class Scraper(object):
         self.df = self.df.loc[:, ['#Shares', 'Value ($)']]
         self.df = self.df.groupby(['Date', 'Ticker']).sum()
         self.df = self.df.reset_index()
+        self.df['Date'] = pd.to_datetime(self.df['Date'], format='%b %d')
+        self.__fix_dates()
         self.df = self.df.set_index('Date')
 
     def split_by_month(self):
         # get months
-        months = []
-        for _, date in enumerate(self.df.index):
-            # date = ind[0]
-            m, d = date.split()
-            if m not in months:
-                months.append(m)
+        months = self.df.index.month_name().unique()
         for month in months:
-            new_df = self.df[self.df.index.str.startswith(month)]
+            month_code = MONTHS[month]
+            new_df = self.df[self.df.index.month == month_code]
             new_df = new_df.reset_index()
             new_df = new_df.set_index(['Date', 'Ticker'])
             try:
-                if month != 'Dec':
+                if (month == 'December') and ('January' in months):
                     old_df = pd.read_csv(
-                        f"Insider_trans_{month}_{self.year}.csv", index_col=['Date', 'Ticker'])
+                        f"Data/Insider_trans_{month}_{self.year - 1}.csv", index_col=['Date', 'Ticker'])
                 else:
                     old_df = pd.read_csv(
-                        f"Insider_trans_{month}_{self.year - 1}.csv", index_col=['Date', 'Ticker'])
-                print('File Found')
-
-                df_combined = old_df.combine(
-                    new_df, lambda a, b: a if np.abs(a) < np.abs(b) else b)
-                if month != 'Dec':
+                        f"Data/Insider_trans_{month}_{self.year}.csv", index_col=['Date', 'Ticker'])
+                df_combined = old_df.append(new_df)
+                df_combined = df_combined.drop_duplicates(keep='last')
+                df_combined.sort_index()
+                if month == 'December' and 'January' in months:
                     df_combined.to_csv(
-                        f"Insider_trans_{month}_{self.year}.csv")
-                else:
+                        f"Data/Insider_trans_{month}_{self.year - 1}.csv")
+                else:  # Handle decembers
                     df_combined.to_csv(
-                        f"Insider_trans_{month}_{self.year - 1}.csv")
-
-            except FileNotFoundError:
-                print('No file found')
-                if month != 'Dec':
-                    new_df.to_csv(
                         f"Data/Insider_trans_{month}_{self.year}.csv")
-                else:
+
+            except FileNotFoundError as f:
+                if (month == 'December') and ('January' in months):
                     new_df.to_csv(
                         f"Data/Insider_trans_{month}_{self.year - 1}.csv")
 
-        print(self.df[self.df.index.str.startswith(months[0])])
-        print(months)
+                else:
+                    new_df.to_csv(
+                        f"Data/Insider_trans_{month}_{self.year}.csv")
+
+    def __fix_dates(self):
+        ind = self.df.loc[:, 'Date']
+        ind = pd.to_datetime(ind, format='%b %d')
+        dates = []
+        for _, date in enumerate(ind):
+            if (date.month == 12) and (datetime.now().month == 1):
+                date = date.replace(year=datetime.now().year - 1)
+            else:
+                date = date.replace(year=datetime.now().year)
+            dates.append(date)
+        self.df['Date'] = dates
+
+    def run(self):
+        print("Beginning to scrape Insider Trading data from Finviz.com")
+        print("================================================================")
+        self.get_df()
+        print("Data gathered from Finviz")
+        print("================================================================")
+        print('Cleaning data...')
+        print("================================================================")
+        self.clean_df()
+        print('Saving data in Data/ directory')
+        print("================================================================")
+        self.split_by_month()
+        print('Done!')
 
 
 if __name__ == '__main__':
-    s = Scraper()
-    s.get_df()
-    s.clean_df()
-    s.split_by_month()
+    Scraper().run()
